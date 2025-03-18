3474912a10123e26d82dc9e734957169ed9ce2a7
StarLight842 <842starlite@gmail.com>
2025-03-18 10:33:20 -0700

    Test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from "astro/config";
-import prefetch from "@astrojs/prefetch";
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), tailwind()],
-  redirects: {
-    "/links": "/",
-    "/faq": "/",
-  },
+  site: "https://aadishv.github.io",
+  base: "aadishv-astro",
 });
