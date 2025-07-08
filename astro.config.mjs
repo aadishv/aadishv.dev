95808e9bb162206c981358c5261081eac4adc936
StarLight842 <842starlite@gmail.com>
2025-07-07 17:08:35 -0700

    style(config): format mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import topLevelAwait from "vite-plugin-top-level-await";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -12,11 +12,21 @@ export default defineConfig({
     }),
   ],
   markdown: {
-      syntaxHighlight: 'shiki', // or 'prism'
-      shikiConfig: {
-        theme: 'github-light', // or 'github-dark', 'nord', etc.
-      },
+    syntaxHighlight: "shiki", // or 'prism'
+    shikiConfig: {
+      theme: "github-light", // or 'github-dark', 'nord', etc.
     },
-  site: 'https://aadishv.github.io',
-  base: ''
+  },
+  site: "https://aadishv.github.io",
+  base: "",
+  vite: {
+      plugins: [
+        topLevelAwait({
+          // The export name of top-level await promise for each chunk module
+          promiseExportName: '__tla',
+          // The function to generate import names of top-level await promise in each chunk module
+          promiseImportName: i => `__tla_${i}`
+        })
+      ]
+    }
 });
