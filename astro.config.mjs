8d57c752eea763eff09532257820e90f59478af4
StarLight842 <842starlite@gmail.com>
2025-09-26 01:17:21 -0700

    feat: heading anchor link handle thingies
    also, opencode trace for last commit: https://opencode.ai/s/W8fH9wz2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,43 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import rehypeSlug from "rehype-slug";
 import mdx from "@astrojs/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), mdx()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
   markdown: {
     syntaxHighlight: "shiki", // or 'prism'
     shikiConfig: {
       theme: "github-light", // or 'github-dark', 'nord', etc.
     },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "text",
+            value: `#`,
+          },
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
   },
   site: "https://aadish.dev",
   base: "",
