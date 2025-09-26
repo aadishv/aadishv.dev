487321513f1058044716dbef24e63091b162b13f
StarLight842 <842starlite@gmail.com>
2025-09-26 09:23:16 -0700

    fix(anchors): `#` no longer shows up in TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: "prepend",
+          behavior: "before",
           content: {
             type: "text",
             value: `#`,
